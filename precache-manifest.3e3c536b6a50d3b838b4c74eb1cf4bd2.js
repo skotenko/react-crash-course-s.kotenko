6f692648d768b9eed17d04b4af1f8640967124ec
Serhii Kotenko <s.kotenko@astoundcommerce.com>
2019-11-07 15:18:01 +0200

    generate build

---
+++ precache-manifest.3e3c536b6a50d3b838b4c74eb1cf4bd2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1baeb8b9bee7eee592e7e09664485724",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9595c7915c73edf09714",
+    "url": "/static/css/main.6f3a2660.chunk.css"
+  },
+  {
+    "revision": "e5d2a5852a25e41ac48b",
+    "url": "/static/js/2.24bfa5a5.chunk.js"
+  },
+  {
+    "revision": "9595c7915c73edf09714",
+    "url": "/static/js/main.0d0101f4.chunk.js"
+  },
+  {
+    "revision": "ff43b92c14f8475f2b42",
+    "url": "/static/js/runtime-main.ba0f74e3.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);

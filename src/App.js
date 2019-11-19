5bca459259d1342cb3227e0bc4d26bdea238cdfd
Serhii Kotenko <s.kotenko@astoundcommerce.com>
2019-11-19 13:53:03 +0200

    import Actions component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Actions from "./components/Actions/Actions";
 import PlanetList from "./components/PlanetList/PlanetList";
 
 class App extends React.Component {
@@ -11,6 +12,7 @@ class App extends React.Component {
           <img src={logo} className="App-logo" alt="logo"/>
         </header>
         <main className="App-main">
+          <Actions/>
           <PlanetList/>
         </main>
       </div>

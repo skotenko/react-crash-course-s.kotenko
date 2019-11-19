72212bf585c716b98a4eafc8e41097209be61418
Serhii Kotenko <s.kotenko@astoundcommerce.com>
2019-11-19 13:52:10 +0200

    refactor changed class components to Hooks

--- src/components/PlanetList/PlanetList.js
+++ src/components/PlanetList/PlanetList.js
@@ -1,87 +1,55 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import axios from 'axios';
-import uuidv1 from 'uuid/v1';
-import PlanetItem from "../PlanetItem/PlanetItem";
-import ErrorIndicator from "../ErrorIndicator/ErrorIndicator";
-import Spinner from "../Spinner/Spinner";
+import PlanetListUI from "./PlanetListUI";
 import './PlanetList.css';
 
-class PlanetList extends React.Component {
+function PlanetList() {
+  let [planets, setPlanets] = useState([]);
+  let [isLoading, setIsLoading] = useState(false);
+  let [isError, setIsError] = useState(false);
+  let [isCancel, setIsCancel] = useState(false);
 
-  baseURL = 'https://swapi.co/api/';
+  const baseURL = 'https://swapi.co/api/';
+  const _source = axios.CancelToken.source();
 
-  /**
-   * Cancel token
-   * @type {null}
-   * @private
-   */
-  _source = null;
+  useEffect(() => {
+    console.log('useEffect');
 
-  state = {
-    isLoading: false,
-    isError: false,
-    isCancel: false,
-    planets: [],
-  };
+    fetchPlanets();
+  }, []);
 
-  onCancelHandler = () => {
-    if (this.state.isLoading) {
-      this.setState({
-        isLoading: false,
-        isCancel: true,
-        isError: false,
-      });
-
-      // Cancel request
-      this._source.cancel();
-
-      setTimeout(() => {
-        this.setState({
-          isLoading: false,
-          isCancel: false,
-          isError: false,
-        });
-      }, 2000);
+  const onCancelHandler = () => {
+    if (isLoading) {
+      setIsLoading(false);
+      setIsCancel(true);
     }
   };
 
-  onSendHandler = () => {
-    if (!this.state.isLoading) {
-      this.setState({
-        isLoading: true,
-        isError: false,
-        isCancel: false,
-        planets: [],
-      });
-
-      this.fetchPlanets();
+  const onSendHandler = () => {
+    if (!isLoading) {
+      fetchPlanets();
     }
   };
 
-  fetchPlanets = () => {
-    this._source = axios.CancelToken.source();
+  function fetchPlanets() {
+    setIsLoading(true);
+    setIsError(false);
+    setIsCancel(false);
 
-    axios.get(`${this.baseURL}planets/`, {cancelToken: this._source.token})
+    axios.get(`${baseURL}planets/`, {cancelToken: _source.token})
       .then(res => {
-        const planets = res.data.results;
-        this.setState({
-          isLoading: false,
-          isError: false,
-          planets: planets,
-        });
+        setPlanets(res.data.results);
+        setIsLoading(false);
       })
       .catch(error => {
+        setIsLoading(false);
 
         if (axios.isCancel(error)) {
           console.info('Request canceled', error.message);
+
+          setIsCancel(true);
         } else {
-          // handle error
-          this.setState({
-            isError: true,
-            isLoading: false,
-            isCancel: false,
-            planets: [],
-          });
+          setIsError(true);
 
           if (error.response) {
             console.error(error.response.data);
@@ -92,46 +60,15 @@ class PlanetList extends React.Component {
           }
         }
       });
-  };
+  }
 
-  render() {
-    const {planets, isLoading, isError, isCancel} = this.state;
-    let text = "Get planets";
-    let disabled = false;
-
-    if (isLoading) {
-      text = "Loading";
-      disabled = true;
-    } else if (isCancel) {
-      text = "Canceled. Get again";
-    } else if (isError) {
-      text = "Error. Try again";
-    }
-
-    const errorMessage = isError ? <ErrorIndicator/> : null;
-    const spinner = isLoading ? <Spinner/> : null;
-
-    return (
-      <div className="PlanetList">
-        <div className="PlanetList__actions">
-          <button onClick={this.onSendHandler} disabled={disabled} className="PlanetList__btn">{text}</button>
-          <button onClick={this.onCancelHandler} className="PlanetList__btn">Cancel Request</button>
-        </div>
-        <h2>The Star Wars Planets:</h2>
-        {errorMessage}
-        {spinner}
-        <ol className="PlanetList__list">
-          {
-            planets.map(item => {
-              const uuid = uuidv1();
-              return <PlanetItem key={uuid} name={item.name}/>;
-            })
-          }
-        </ol>
-
-      </div>
-    );
-  };
+  return (
+    <PlanetListUI isError={isError}
+                  isLoading={isLoading}
+                  isCancel={isCancel}
+                  planets={planets}
+    />
+  );
 }
 
 export default PlanetList;
